7fc241afb5a5d7e05429aa5ba4ff57d4f968eb87
zero9501 <15133024857@163.com>
2024-08-07 17:54:32 +0800

    feature: add log api

--- spoor.go
+++ spoor.go
@@ -7,46 +7,72 @@
 
 package spoor
 
-import "go.uber.org/zap"
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type logLevel zapcore.Level
+
+const (
+	DEBUG = zapcore.DebugLevel
+	INFO  = zapcore.InfoLevel
+	WARN  = zapcore.WarnLevel
+	ERROR = zapcore.ErrorLevel
+	PANIC = zapcore.PanicLevel
+	FATAL = zapcore.FatalLevel
+)
 
 type Spoor struct {
 	*zap.Logger
 }
 
-func (sp *Spoor) Info(s string) {
-	sp.Logger.Info(s)
+func (sp *Spoor) Log(level logLevel, s string, fields ...zap.Field) {
+	sp.Logger.Log(zapcore.Level(level), s, fields...)
+}
+
+func (sp *Spoor) LogSf(level logLevel, s string, v ...interface{}) {
+	sp.Logger.Sugar().Logf(zapcore.Level(level), s, v)
+}
+
+func (sp *Spoor) Info(s string, fields ...zap.Field) {
+	sp.Logger.Info(s, fields...)
 }
 
 func (sp *Spoor) InfoSf(s string, v ...interface{}) {
 	sp.Sugar().Infof(s, v...)
 }
 
-func (sp *Spoor) Debug(s string) {
-	sp.Logger.Debug(s)
+func (sp *Spoor) Debug(s string, fields ...zap.Field) {
+	sp.Logger.Debug(s, fields...)
 }
 
 func (sp *Spoor) DebugSf(s string, v ...interface{}) {
 	sp.Sugar().Debugf(s, v...)
 }
 
-func (sp *Spoor) Error(s string) {
-	sp.Logger.Error(s)
+func (sp *Spoor) Error(s string, fields ...zap.Field) {
+	sp.Logger.Error(s, fields...)
 }
 
 func (sp *Spoor) ErrorSf(s string, v ...interface{}) {
 	sp.Sugar().Errorf(s, v...)
 }
 
-func (sp *Spoor) Panic(s string) {
-	sp.Logger.Panic(s)
+func (sp *Spoor) Panic(s string, fields ...zap.Field) {
+	sp.Logger.Panic(s, fields...)
+}
+
+func (sp *Spoor) DPanic(s string, fields ...zap.Field) {
+	sp.Logger.DPanic(s, fields...)
 }
 
 func (sp *Spoor) PanicSf(s string, v ...interface{}) {
 	sp.Sugar().Panicf(s, v...)
 }
 
-func (sp *Spoor) Fatal(s string) {
-	sp.Logger.Fatal(s)
+func (sp *Spoor) Fatal(s string, fields ...zap.Field) {
+	sp.Logger.Fatal(s, fields...)
 }
 
 func (sp *Spoor) FatalSf(s string, v ...interface{}) {
